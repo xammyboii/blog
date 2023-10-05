f76f4f94d8f1cc052b8f20b52a26e8588f7d504b
xammyboii <xavieryabados.09@gmail.com>
2023-10-04 21:20:06 -0500

    7.1 added route for displaying a single article

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
-  get "/articles", to: "articles#index"
+
+  get "/articles",     to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 end
