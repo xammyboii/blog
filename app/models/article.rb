a59df02b57e9ccc632a37abe20945d7ce63bcf43
xammyboii <xavieryabados.09@gmail.com>
2023-10-04 21:54:35 -0500

    7.3.3 added validations for adding a new article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
+# Name: Xam Abados
+# Course: WEBD-3008 Full-Stack Web Development
+# Date Created: 10.04.2023
+# Project: Challenge 4 - Blog
+
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
