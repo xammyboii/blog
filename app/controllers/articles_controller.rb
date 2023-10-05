edd0d1d5dfcdfb94717f829276f215d47a3bc721
xammyboii <xavieryabados.09@gmail.com>
2023-10-04 21:09:03 -0500

    added index action to fetch all articles from db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
+# Name: Xam Abados
+# Course: WEBD-3008 Full-Stack Web Development
+# Date Created: 10.04.2023
+# Project: Challenge 4 - Blog
+
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 end
