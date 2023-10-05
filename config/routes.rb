b32a140cff3654085289eb44fb3bdd6eca2f807a
xammyboii <xavieryabados.09@gmail.com>
2023-10-04 20:33:18 -0500

    added route to articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+# Name: Xam Abados
+# Course: WEBD-3008 Full-Stack Web Development
+# Date Created: 10.04.2023
+# Project: Challenge 4 - Blog
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/articles", to: "articles#index"
 end
