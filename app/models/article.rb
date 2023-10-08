ffd27a6be89740c36b4f2c14cd7df1915df518f9
xammyboii <xavieryabados.09@gmail.com>
2023-10-08 18:24:23 -0500

    added routes for comments to have full association with articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,8 @@
 # Project: Challenge 4 - Blog
 
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
