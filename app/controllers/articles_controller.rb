be13cb7204d586422088f03c0161a989f20cc3d9
xammyboii <xavieryabados.09@gmail.com>
2023-10-08 19:16:14 -0500

    added security for when creating new article and commenting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,8 @@
 # Project: Challenge 4 - Blog
 
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
